c99c3e66afa540956f08eb95205a332714237b40
afulvio@gmail.com <afulvio@gmail.com@249fe887-37af-4c22-3e2b-7d3a915e8dff>
2012-01-25 10:50:16 +0000

    new simple example "class"

--- require.inc.php
+++ require.inc.php
@@ -1,2 +1,10 @@
 <?php
+
+// local debug configuration
+$conn = dirname(__FILE__).'/conf/conn.inc.php';
+if (file_exists($conn)) {
+	define('_JAMP_SETTING_',		dirname(__FILE__).'/conf/setting.inc.php');
+	define('_JAMP_CONNECTIONS_',	$conn);
+}
+
 require(dirname(__FILE__).'/../jamp/jamp.inc.php');
