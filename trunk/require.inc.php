20677a4874e83520aec20ad37e16db7fb3a31e98
giampiero.ancilletta@poste.it <giampiero.ancilletta@poste.it@249fe887-37af-4c22-3e2b-7d3a915e8dff>
2012-01-29 22:37:17 +0000

    Modify require jamp.inc.php

--- trunk/require.inc.php
+++ trunk/require.inc.php
@@ -3,8 +3,9 @@
 // local debug configuration
 $conn = dirname(__FILE__).'/conf/conn.inc.php';
 if (file_exists($conn)) {
-	define('_JAMP_SETTING_',		dirname(__FILE__).'/conf/setting.inc.php');
-	define('_JAMP_CONNECTIONS_',	$conn);
+	define('_JAMP_SETTING_', dirname(__FILE__).'/conf/setting.inc.php');
+	define('_JAMP_CONNECTIONS_', $conn);
 }
 
-require(dirname(__FILE__).'/../jamp/jamp.inc.php');
+require(dirname(__FILE__).'/../jamp.inc.php');
+?>
